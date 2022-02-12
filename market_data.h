ac97cbc9a57c597a52a39702881cdc308b2d10f7
Mikhail Fedyakov <mf_52@mail.ru>
2022-02-12 12:09:37 +0300

    market_data

--- market_data.h
+++ market_data.h
@@ -2,14 +2,21 @@
 
 #include "types.h"
 
-struct MarketDataPacketHeader {
-    u32 msg_seq_number {};
-    u16 msg_size {};
-    u16 msg_flags {};
-    u64 sending_time {};
+struct MarketDataPacketHeader { // Little endian
+    u32 msg_seq_number {};          // Счётчик. ++ когда отправляет сообщение. Ресет раз в сутки
+    u16 msg_size {};                // Длина сообщения в байтах
+    u16 msg_flags {};               // Флаги. Лучше читать в доке про флаги
+    u64 sending_time {};            // Время отправки сообщения шлюзом
 };
 
-struct IncreamentalPacketHeader {
-
+struct IncreamentalPacketHeader { // Little endian
+    u64 transact_time {};               // Начало процесинга транзакции в матчинге с момента начала эпохи
+    u32 exchange_trading_session_ID {}; // ID торговой сессии
 };
 
+struct SBEHeader {
+    u16 block_length {};  // Длина корневой части сообщения, кроме самой SBE Header и NoMDEntries
+    u16 template_ID {}; // ID сообщения
+    u16 schema_ID {};   // ID схемы сообщения
+    u16 version {};     // Версия схемы
+};
