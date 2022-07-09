eee07144acc160119ebb5bdff47123745b75ec28
Mikhail Fedyakov <mf_52@mail.ru>
2022-07-09 15:56:09 +0300

    better argparse

--- README.md
+++ README.md
@@ -9,3 +9,17 @@ PCAP contains samples of SIMBA SPECTRA traffic captured on production system.
 Tested for:
 
 http://ftp.moex.ru/pub/SIMBA/Spectra/prod/pcap/2021-11-10.1844-1910.zip
+
+
+Some usefull information:
+
+
+About PCAP format:
+https://tools.ietf.org/id/draft-gharris-opsawg-pcap-00.html
+
+
+About MOEX protocol:
+
+http://ftp.moex.com/pub/SIMBA/Spectra/test/doc/spectra_simba_en.pdf
+
+http://ftp.moex.com/pub/SIMBA/Spectra/test/doc/spectra_simba_ru.pdf
--- app/main.cpp
+++ app/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <binary_parser/buffered_reader.h>
+#include <argparse/argparse.hpp>
 
 #include "global_pcap_header.h"
 #include "record_header.h"
@@ -7,14 +8,60 @@
 #include "udp_header.h"
 #include "moex/market_data_packet.h"
 
-const std::string encoded_file_path = "../../assets/sample.pcap";
+struct Config {
+    std::string encoded_file_path;
+    std::string decoded_file_path;
+    size_t packets_to_parse;
 
-int main() {
-    BufferedReader parser{encoded_file_path};
+    Config(int argc, char* argv[]) {
+        argparse::ArgumentParser a("MOEX Simba parser");
+        a.add_argument("-p", "--path")
+                .help("Encoded pcap file path")
+                .required();
+        a.add_argument("-o", "--out")
+                .default_value<std::string>("../../decoded.txt")
+                .help("Decoded .txt file path")
+                .required();
+        a.add_argument("-n", "--n-packets-decode")
+                .default_value<int>(42)
+                .scan<'i', int>()
+                .help("Number of packets to decode")
+                .required();
+        try {
+            a.parse_args(argc, argv);
+        }
+        catch (const std::runtime_error& err) {
+            std::cerr << err.what() << std::endl;
+            std::cerr << a;
+            std::exit(1);
+        }
+
+        encoded_file_path = a.get("-p");
+        decoded_file_path = a.get("-o");
+        packets_to_parse = a.get<int>("-n");
+    }
+
+    template<class O> friend O& operator<<(O& out, const Config& config) {
+        out << fmt::format(
+                "Decoded file path: {}\n"
+                "Encoded file path: {}\n"
+                "Number of packets to decode: {}\n",
+                config.decoded_file_path,
+                config.encoded_file_path,
+                config.packets_to_parse
+        );
+        return out;
+    }
+};
+
+int main(int argc, char* argv[]) {
+    Config config = Config(argc, argv);
+
+    BufferedReader parser{config.encoded_file_path};
     GlobalPcapHeader global_pcap_header{parser};
     std::cout << global_pcap_header << '\n';
 
-    for (int i = 0; i < 1000000; i++) {
+    for (int i = 0; i < config.packets_to_parse; i++) {
 
         std::cout << "Packet number " << i+1 << '\n';
         RecordHeader record_header{parser, global_pcap_header.magic_number.is_ns()}; // TODO pass config or smth like that
